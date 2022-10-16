347bdaed2269a35a7f76c4c000e0dd22b9bbc6f7
Sharoz Raees <52934734+sharozraees802@users.noreply.github.com>
2022-10-16 13:54:41 +0500

    default time out, in memory , pattern
    the default time out in the cypress config file because of some time out error for internet speed 
    numTestsKeptInMemory: test run to create cash garbage add this command to handle the memory side
    pattern space: specific file create like js,jsx,ts,tsx

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   projectId: 'v2x96h',
+  numTestsKeptInMemory: 0,
+  defaultCommandTimeout: 60000,
   e2e: {
     setupNodeEvents(on, config) {
     },
     baseUrl: 'http://localhost:3003',
-    experimentalSessionAndOrigin: true
+    experimentalSessionAndOrigin: true,
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    numTestsKeptInMemory: 0,
   },
 })
