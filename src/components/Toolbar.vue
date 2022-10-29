3c2d086734d47a63821c2480ae95eac279616070
Filip Hric <filip@filiphric.com>
2022-10-29 20:54:14 +0200

    poc: add a toolbar into test runner

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="copyLink">Hey there</button>
+</template>
+<script setup lang="ts">
+
+const allProps = defineProps({
+  props: {
+    default: [],
+    type: Object
+  }
+});
+
+const copyLink = async () => {
+  const { props }: any = allProps
+  const type = typeof props
+  const clipBoardValue = type === 'object' ? JSON.stringify(props, null, 2) : props
+  await parent.navigator.clipboard.writeText(clipBoardValue);
+}
+
+</script>
