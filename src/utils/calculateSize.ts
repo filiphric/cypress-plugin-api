c9e17b487a9e3ad2b8b795b39f0cc5d6494a680e
Filip Hric <filip@filiphric.com>
2022-11-07 11:09:43 +0100

    feat: folding of objects and arrays in JSON file

--- src/utils/calculateSize.ts
+++ src/utils/calculateSize.ts
@@ -1,21 +1,14 @@
+import { isStringValidJson } from "./isStringValidJson"
+
 export const calculateSize = (value: any) => {
 
   const stringified = value.toString()
   // remove all carriage return symbols
   const cleanString = stringified.replace(/\r\n/g, '\n')
   // replace white spaces if value is a JSON
-  const finalString = isJsonString(cleanString) ? cleanString.replace(/\s/g, '') : cleanString
+  const finalString = isStringValidJson(cleanString) ? cleanString.replace(/\s/g, '') : cleanString
   const byteCount = new Blob([finalString]).size
 
   return byteCount
 
-}
-
-function isJsonString(str: string) {
-  try {
-    JSON.parse(str);
-  } catch (e) {
-    return false;
-  }
-  return true;
 }
--- src/utils/transform.ts
+++ src/utils/transform.ts
@@ -1,16 +1,25 @@
 import * as Prism from 'prismjs';
 import 'prismjs/components/prism-json';
-import 'prismjs/plugins/line-numbers/prism-line-numbers';
-
-// import 'prism-js-fold'
+import { isStringValidJson } from './isStringValidJson';
 
 export const transform = (body: any, language: 'json' | 'html' | 'xml' | 'plaintext' = 'json') => {
   let content = language === 'json' ? JSON.stringify(body, null, 2) : body
   if (body) {
     let formatted = Prism.highlight(content, Prism.languages[language], language)
-    const code = formatted.split('\n')
-      .map((line, num) => `<span class="line-number text-slate-700 select-none">${(num + 1).toString().padStart(4, ' ')}  </span>${line}`)
+
+    let code = formatted.split('\n')
+      .map((line, num) => `<span class="line-number text-slate-700 select-none contents align-top">${(num + 1).toString().padStart(4, ' ')}  </span>${line}`)
       .join('\n');
+
+
+    // add folding to every json object and array
+    if (isStringValidJson(content)) {
+      code = code
+        .replaceAll('<span class="token punctuation">{</span>', '<details class="contents" open><summary class="inline-block brace"><span class="token punctuation">{</span></summary>')
+        .replaceAll('<span class="token punctuation">[</span>', '<details class="contents" open><summary class="inline-block bracket"><span class="token punctuation">[</span></summary>')
+        .replaceAll('<span class="token punctuation">}</span>', '</details><span class="token punctuation inline-block">}</span>')
+        .replaceAll('<span class="token punctuation">]</span>', '</details><span class="token punctuation inline-block">]</span>')
+    }
 
     return `<code class="language-${language}">${code}</code>`
   }
