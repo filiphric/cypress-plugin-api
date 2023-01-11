3c240b15c3c01a78da95679959d7da0f1dbb100a
Filip Hric <filip@filiphric.com>
2023-01-11 23:10:49 +0100

    fix: displaying of url when request is sent to IP address

--- src/modules/cloneProps.ts
+++ src/modules/cloneProps.ts
@@ -1,10 +1,10 @@
-import { isValidUrl } from "@utils/isValidUrl"
+import { isValidUrlOrIp } from "@utils/isValidUrlOrIp"
 import { ApiRequestOptions, RequestProps } from "src/types"
 const { _ } = Cypress
 
 export const cloneProps = (props: RequestProps[], index: number, options: ApiRequestOptions) => {
   props[index].method = _.cloneDeep(options.method) || 'GET'
-  props[index].url = isValidUrl(options.url) ? options.url : Cypress.config('baseUrl') + options.url
+  props[index].url = isValidUrlOrIp(options.url) ? options.url : Cypress.config('baseUrl') + options.url
   props[index].query.body = _.cloneDeep(options.qs)
   props[index].auth.body = _.cloneDeep(options.auth)
   props[index].requestHeaders.body = _.cloneDeep(options.headers)
